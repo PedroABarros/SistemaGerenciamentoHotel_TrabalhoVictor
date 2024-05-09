742a1a2302053e8786f717d9cc0573fd50681142
Jackson <jacksonaparesido@gmail.com>
2024-05-09 15:11:51 -0300

    service hotel

--- HotelJPJ/src/main/java/com/hoteljpj/controller/HotelController.java
+++ HotelJPJ/src/main/java/com/hoteljpj/controller/HotelController.java
@@ -4,6 +4,7 @@ import com.hoteljpj.model.entity.Hotel;
 import com.hoteljpj.model.repository.HotelRepository;
 import com.hoteljpj.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,10 +33,11 @@ public class HotelController {
     // CRIA UM NOVO HOTEL
     // QUANDO SE USA O METODO POST
     // QUANDO SE USA O PUT ELE VAI EDITAR UMA JÁ EXISTENTE
-    // MAIS PARA ISSO TEM Q SER PASSADO O ID
-    // EX: http://localhost:8080/api/hotel/{id}
+    // EX: PUT
+    // http://localhost:8080/api/hotel
     // NESSE EXEMPLO ELE IRA EDITAR
-    // EX: http://localhost:8080/api/hotel/
+    // EX: POST
+    //     http://localhost:8080/api/hotel
     // NESSE OUTRO ELE IRA CRIAR UM NOVO
 
     @PostMapping()
@@ -55,28 +57,52 @@ public class HotelController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-    @GetMapping("/getById/{id}")
-    public Optional<Hotel> getById(@PathVariable("id") long id) {
-        return hotelRepository.findById(id);
+
+    // NO findById ELE IRA ACHAR UM HOTEL
+    // É APENAS NECESSARIO PASSAR UM ID
+    // CASO NÃO PASSE ELE IRA RETORNAR
+    // UMA LISTA COM OS HOTEIS CADASTRADO
+    // EX: GET
+    //     http://localhost:8080/api/hotel/1
+
+    @GetMapping("{id}")
+    public ResponseEntity findById(@PathVariable("id") Long id) {
+        try {
+            return ResponseEntity.ok(hotelService.findById(id));
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
+    // AQUI RETORNA A LISTA DOS HOTEIS
+    // EX: GET
+    //     http://localhost:8080/api/hotel
+    @GetMapping()
+    public ResponseEntity findAll() {
+        return ResponseEntity.ok(hotelService.findAll());
+    }
+    // NO METODO DELTE ELE  APENAS
+    // VAI DELETAR OS OBJETOS
+    // EX: DELETE
+    //     http://localhost:8080/api/hotel
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        try {
+            return ResponseEntity.ok(hotelService.delete(id));
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+    // APENAS RETORNA O NUMERO TOTAL DE HOTEIS
+    // EX : GET
+    //      http://localhost:8080/api/hotel/total
     @GetMapping("/total")
     public Long getTotal() {
         return hotelRepository.count();
     }
-
-    @DeleteMapping("/delete/{id}")
-    public Hotel delete(@PathVariable("id") long id) {
-        Optional<Hotel> hotelemover = hotelRepository.findById(id);
-        if (!hotelemover.isPresent()) return null;
-
-        hotelRepository.delete(hotelemover.get());
-        return hotelemover.get();
+}
 
 
-    }
 
-
-    }
 
 
--- HotelJPJ/src/main/java/com/hoteljpj/service/HotelService.java
+++ HotelJPJ/src/main/java/com/hoteljpj/service/HotelService.java
@@ -3,6 +3,9 @@ package com.hoteljpj.service;
 import com.hoteljpj.model.entity.Hotel;
 import com.hoteljpj.model.repository.HotelRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,13 +32,32 @@ public class HotelService {
 
     public Hotel save(Hotel hotel) throws Exception {
         if (hotel.getClassificacao() == null || hotel.getClassificacao() > 5 || hotel.getClassificacao() < 0) {
-            System.out.println("Valor da classificação: " + hotel.getClassificacao());
             throw new Exception("Classificação não pode ser menor que 0 ou maior que 5");
         }
-
         if (hotel.getNome() == null || hotel.getNome().length() < 3) {
             throw new Exception("Nome deve ter pelo menos 3 caracteres.");
         }
+        // TESTAR NA FACULDADE
+        // O DA DATA EU ACHO Q VAI DAR ERRADO
+        if (hotel.getQtdQuartos() <= 0 || hotel.getQtdQuartos() == null){
+            throw new Exception("Quantidade Quartos não pode ser menor que 1 ou nulo");
+        }
+        Date hoje = new Date();
+        if (hotel.getDataFundacao().before(hoje)) {
+            throw new IllegalStateException("A DataFundacao do Hotel não pode ser menor que a data do dia do cadastro");
+        }
+        if (hotel.getCafe() == null) {
+            throw new Exception("Cafe do Hotel não pode ser nulo.");
+        }
+
+        if (hotel.getAlmoco() == null) {
+            throw new Exception("Almoco do Hotel não pode ser nulo.");
+        }
+
+        if (hotel.getJanta() == null) {
+            throw new Exception("Janta do Hotel não pode ser nula.");
+        }
+
         return hotelRepository.save(hotel);
     }
 
@@ -55,3 +77,10 @@ public class HotelService {
     }
 
 }
+//   JEAN E PEDRO, ESSE CODIGO AQUI E SÓ PRA LEMBRA DE COLOCAR NO QUARTO,
+//   PARA VERIFICA SE ELE TA COMEÇANDO COM LETRA QUE NEM NAS RF
+//
+//                  Nova validação para Identificacao do Quarto
+//            if (hotel.getId() == null || !hotel.getIdentificacao().matches("^[a-zA-Z]\\d{4,}$")) {
+//                throw new Exception("A Identificacao do Quarto deve conter no mínimo 5 dígitos, começando com uma letra.");
+//            }
